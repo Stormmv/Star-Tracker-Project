435172d9dce1835a39ade72f51843e58c79ff4fa
Stormmv <xboxstormmv@gmail.com>
2024-10-07 02:52:13 +1300

    speed change

--- src/global.cpp
+++ src/global.cpp
@@ -13,7 +13,7 @@ float initialAngle = 0.0;
 //motor controller
 int acceleration = 20000;
 int maxSpeed = 200;
-int speed = 146;
+int speed = 152;
 int steps = 200;
 
 //====================================================================================================
